7ca867fa5f6cfe8648b2965c8e76ed862a55ce4f
MFMitch <61880515+MFMitch@users.noreply.github.com>
2020-09-13 11:49:43 -0500

    Ball Game Update
    Fixed the balls movement and cleaned up the code a bit

--- Assets/Script/BallController.cs
+++ Assets/Script/BallController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
@@ -9,7 +10,7 @@ public class BallController : MonoBehaviour
     public eSelectionState _state;
 
     public Rigidbody _rb;
-
+    private int BallFireForce = 40;
 
     public GameObject PREFAB;
     public int distance = 1;
@@ -19,48 +20,61 @@ public class BallController : MonoBehaviour
 
         if (_state == eSelectionState.FREE)
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out hit, 100.0f))
-                {
-                    Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
-                    _state = eSelectionState.SELECTED;
-                }
-            }
+            TickFree();
         }
         else if (_state == eSelectionState.SELECTED)
         {
-            //draw line
+            TickSelected();
+        }
+    }
+    private void TickSelected()
+    {
+        Vector3 point = GetPointUnderMouse();
+
+        Vector3 lineAngle = (point - transform.position);
+        //Vector3 lineAngle = this.transform.position - point;
+
+        // GameObject.Instantiate(PREFAB, point, Quaternion.identity);
+
+
+        Vector3 direction = new Vector3(lineAngle.x, this.transform.position.y, lineAngle.z).normalized;
+
+        Debug.DrawLine(this.transform.position, direction, Color.red, 10);
+
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            ShootBallInDirection(direction);
+            _state = eSelectionState.FREE;
+        }
+    }
+    private void TickFree()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Physics.Raycast(ray, out hit, float.PositiveInfinity);
-            Vector3 point = new Vector3(hit.point.x, hit.point.y, hit.point.z);
-            
-            Vector3 lineAngle = ((point * distance) * -1).normalized;
-            //Vector3 lineAngle = this.transform.position - point;
-
-            // GameObject.Instantiate(PREFAB, point, Quaternion.identity);
-
-
-            Vector3 direction = new Vector3(lineAngle.x, this.transform.position.y, lineAngle.z).normalized;
-
-            Debug.Log("Point: " + point.ToString());
-            Debug.Log("LineAngle : " + lineAngle.ToString());
-            Debug.Log("Dir : " + direction.ToString());
-
-
-            Debug.DrawLine(this.transform.position, direction, Color.red, 10);
-
-
-            if (Input.GetMouseButtonUp(0))
+            if (Physics.Raycast(ray, out hit, 100.0f))
             {
-                //Shoot the ball
-                _rb.AddForce(lineAngle * 2, ForceMode.Force);
-                //_rb.AddForceAtPosition(lineAngle*10, this.transform.position, ForceMode.Acceleration);
-                _state = eSelectionState.FREE;
+                Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+                _state = eSelectionState.SELECTED;
             }
         }
+    }    
+    private Vector3 GetPointUnderMouse()
+    {
+        //draw line
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out hit, float.PositiveInfinity);
+        return hit.point;
+    }
+
+    private void ShootBallInDirection(Vector3 direction)
+    {
+        //Shoot the ball
+        print("Shooting ball twards: " + direction);
+        _rb.AddForce(direction * BallFireForce, ForceMode.Impulse);
+        //_rb.AddForceAtPosition(lineAngle*10, this.transform.position, ForceMode.Acceleration);
     }
 }
